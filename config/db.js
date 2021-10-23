28a7a4b1198d4882bea233aa0808b822554cb426
chetan-spec <biggbosschetan@gmail.com>
2021-10-23 18:18:26 +0530

    6

---
+++ config/db.js
@@ -0,0 +1,21 @@
+require('dotenv').config({ path: __dirname + '/.env' });
+const mongoose = require('mongoose');
+const db = process.env.mongoURI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log('Database Connected.');
+  } catch (err) {
+    console.log(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
